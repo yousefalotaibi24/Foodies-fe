b0ec5f7a90a03d19665a2761992c8d00d8b0d21f
yousefalotaibi24 <yousefalotaibi21@gmail.com>
2024-12-16 18:56:17 +0300

    m

---
+++ src/Components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+function Nav() {
+  return (
+    <nav>
+      <NavLink to="/">Home"</NavLink>
+      <NavLink to="/">Home"</NavLink>
+      <NavLink to="/">Home"</NavLink>
+    </nav>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Home from "./Components/Home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/Home",
+    element: <Home />,
+  },
+  {
+    path: "/Login",
+    element: <login />,
+  },
+]);
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
